887893d30c95eb4c04445cd804de08164cb4b505
Tom Gray <tomgray@Macbook-Tom.lan>
2020-11-26 21:40:38 +1100

    fixin' the url

--- sentiment-analysis-frontend/src/App.js
+++ sentiment-analysis-frontend/src/App.js
@@ -9,8 +9,6 @@ import Polarity from "./components/Polarity";
 const style = {
     marginLeft: 12,
 };
-// if API_BASE_URL is not nil then use it's value, otherwise fall back to the default hardcoded url.
-const apiUrl = process.env.API_BASE_URL ? process.env.API_BASE_URL : 'http://localhost:8080/sentiment';
 
 class App extends Component {
     constructor(props) {
@@ -22,12 +20,20 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch(apiUrl, { 
+        if (window.location.hostname === 'http://sa.info') {
+            var apiUrl = 'http://sa.info/sentiment'
+        } else {
+            var apiUrl = 'http://localhost:8080/sentiment'
+        }
+        console.log("domain detected as: " + window.location.hostname )
+        console.log("fetching api at " + apiUrl)
+
+        fetch(apiUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({sentence: this.textField.getValue()})
+            body: JSON.stringify({ sentence: this.textField.getValue() })
         })
             .then(response => response.json())
             .then(data => this.setState(data));
@@ -41,7 +47,7 @@ class App extends Component {
 
     render() {
         const polarityComponent = this.state.polarity !== undefined ?
-            <Polarity sentence={this.state.sentence} polarity={this.state.polarity}/> :
+            <Polarity sentence={this.state.sentence} polarity={this.state.polarity} /> :
             null;
 
         return (
@@ -50,8 +56,8 @@ class App extends Component {
                     <Paper zDepth={1} className="content">
                         <h2>Sentiment Analyser</h2>
                         <TextField ref={ref => this.textField = ref} onKeyUp={this.onEnterPress.bind(this)}
-                                   hintText="Type your sentence."/>
-                        <RaisedButton  label="Send" style={style} onClick={this.analyzeSentence.bind(this)}/>
+                            hintText="Type your sentence." />
+                        <RaisedButton label="Send" style={style} onClick={this.analyzeSentence.bind(this)} />
                         {polarityComponent}
                     </Paper>
                 </div>
